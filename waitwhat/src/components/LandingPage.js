7672245de985235dca33b00bf6d73e9418e803ca
EvanDAB <carr916@gmail.com>
2021-02-10 14:45:20 -0800

    started landing page, still need to finish social nav

--- waitwhat/src/components/LandingPage.js
+++ waitwhat/src/components/LandingPage.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Row, Col, ResponsiveEmbed } from 'react-bootstrap';
 
 class LandingPage extends React.Component {
     render() {
-        return <h1>WaitWhat</h1>;
+        return (
+            <div id='landing-page-container'>
+                <Row id='music-videos-section'>
+                    <Col>
+                        <ResponsiveEmbed aspectRatio="16by9">
+                            <iframe width="441" height="248" src="https://www.youtube.com/embed/EtzXluKcwSU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </ResponsiveEmbed>
+                    </Col>
+                    <Col></Col>
+                </Row>
+                <Row id='image-gallery'>
+                    <Col></Col>
+                </Row>
+                <Row id='social-section'>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+                <Row id='sign-up-section'>
+                <Col></Col>
+                </Row>
+            </div>
+        );
     }
 }
 
--- waitwhat/src/components/NavBar.js
+++ waitwhat/src/components/NavBar.js
@@ -1,41 +1,53 @@
 import React from 'react';
 import logo from '../waitwhattext.png';
 import { Nav, Navbar, NavDropdown, Row, Col,  } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import LandingPage from './LandingPage';
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.routerRef = React.createRef();
+        this.navbarRef = React.createRef();
+    }
     render() {
         return (
-            <Row>
-                <Navbar>
-                    <Col className='justify-content-left' md={4}>
-                    </Col>
-                    <Col md={4}>
-                        <Navbar.Brand className='justify-content-center'>
-                            <img
-                            alt='“Wait, what?” is at the root of all understanding.'
-                            src={logo}
-                            width='80%'
-                            height='50%'
-                            />
-                        </Navbar.Brand>
-                    </Col>
-                    <Col className='justify-content-right' md={4}>
-                        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-                        <Navbar.Collapse id='basic-navbar-nav'>
-                            <Nav className='ml-auto'>
-                                <NavDropdown title='Music' id='basic-nav-dropdown'>
-                                    <NavDropdown.Item href='#music/original'>Our Music</NavDropdown.Item>
-                                    <NavDropdown.Item href='#music/resources'>Resources</NavDropdown.Item>
-                                </NavDropdown>
-                                <Nav.Link href='#merch'>Merch</Nav.Link>
-                                <Nav.Link href='#bio'>Bio</Nav.Link>
-                                <Nav.Link href='#pressKit'>Press Kit</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Col>
-                </Navbar>
-            </Row>
+            <BrowserRouter ref={this.routerRef}>
+                <Row className=''>
+                    <Navbar ref={this.navbarRef}>
+                        <Col className='justify-content-left' md={4}>
+                        </Col>
+                        <Col md={4}>
+                            <Navbar.Brand className='justify-content-center' href=''>
+                                <img
+                                alt='“Wait, what?” is at the root of all understanding.'
+                                src={logo}
+                                width='80%'
+                                height='50%'
+                                />
+                            </Navbar.Brand>
+                        </Col>
+                        <Col className='justify-content-right' md={4}>
+                            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                            <Navbar.Collapse id='basic-navbar-nav'>
+                                <Nav className='ml-auto'>
+                                    <NavDropdown title='Music' id='basic-nav-dropdown'>
+                                        <NavDropdown.Item href='#music/original'>Our Music</NavDropdown.Item>
+                                        <NavDropdown.Item href='#music/resources'>Resources</NavDropdown.Item>
+                                    </NavDropdown>
+                                    <Nav.Link href='#merch'>Merch</Nav.Link>
+                                    <Nav.Link href='#bio'>Bio</Nav.Link>
+                                    <Nav.Link href='#pressKit'>Press Kit</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Col>
+                    </Navbar>
+                </Row>
+                <Switch>
+                    <Route exact path='' component={LandingPage}/>
+                </Switch>
+            </BrowserRouter>
+            
        
         );
     }
